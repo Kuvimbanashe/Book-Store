ac41fe47cc032f232bd6954398224499eef0c11d
Kuvimbanashe <126201302+Kuvimbanashe@users.noreply.github.com>
2024-10-25 15:50:35 +0200

    removed the backend to a separate folder

--- backend/config.js
+++
@@ -1,6 +0,0 @@
-export const PORT = 5555;
-export const DbConnection = 'mongodb+srv://camaradery303:sDcEC8J4.TV_TK6@bookstore.3c9z2.mongodb.net/?retryWrites=true&w=majority&appName=bookstore'
-export const JWT_SECRET = 'your-secret-key';
-export const JWT_EXPIRE = '30d';
-export const JWT_COOKIE_EXPIRE = 30;
-
